0eb180494edfe5dacc1b46ab4721cdb520cd4bb4
shitakemura <shitakemura@gmail.com>
2022-03-20 19:42:00 +0900

    update stack

--- hello-ecs/lib/hello-ecs-stack.ts
+++ hello-ecs/lib/hello-ecs-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { ContainerImage } from "aws-cdk-lib/aws-ecs";
+import { Vpc } from "aws-cdk-lib/aws-ec2";
+import { ContainerImage, Cluster } from "aws-cdk-lib/aws-ecs";
 import { ApplicationLoadBalancedFargateService } from "aws-cdk-lib/aws-ecs-patterns";
 
 export class HelloEcsStack extends Stack {
@@ -8,10 +9,20 @@ export class HelloEcsStack extends Stack {
     super(scope, id, props);
 
     // VPC
+    const vpc = new Vpc(this, "HelloEcsVpc", {
+      maxAzs: 2,
+    });
 
     // Cluster
+    const cluster = new Cluster(this, "HelloEcsCluster", {
+      vpc: vpc,
+    });
 
-    new ApplicationLoadBalancedFargateService(this, "MyWebServer", {
+    // Load-balanced Fargate Service
+    new ApplicationLoadBalancedFargateService(this, "HelloEcsFargateService", {
+      cluster: cluster,
+      cpu: 256,
+      desiredCount: 2,
       taskImageOptions: {
         image: ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
       },
