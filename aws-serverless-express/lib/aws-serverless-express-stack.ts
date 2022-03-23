8c0229c9b9542f532f629d2ef470ad0071b90bdb
shitakemura <shitakemura@gmail.com>
2022-03-23 21:13:40 +0900

    feat: implement cdk stack

--- aws-serverless-express/lib/aws-serverless-express-stack.ts
+++ aws-serverless-express/lib/aws-serverless-express-stack.ts
@@ -1,16 +1,43 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { CorsHttpMethod, HttpApi } from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import * as path from 'path';
 
 export class AwsServerlessExpressStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Lambda
+    const serverlessLambda = new NodejsFunction(this, 'serverless-lambda', {
+      entry: path.join(__dirname, '../lambda/app.ts'),
+      handler: 'handler',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsServerlessExpressQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // HTTP API Gateway
+    const serverlessLambdaApi = new HttpApi(this, 'serverless-lambda-api', {
+      corsPreflight: {
+        allowMethods: [CorsHttpMethod.OPTIONS, CorsHttpMethod.GET],
+        allowOrigins: ['http://localhost:3000'],
+      },
+    });
+
+    // HTTP Lambda Integration
+    const serverlessLambdaIntegration = new HttpLambdaIntegration(
+      'serverless-lambda-integration',
+      serverlessLambda
+    );
+
+    // API Routes
+    serverlessLambdaApi.addRoutes({
+      path: '/{proxy+}',
+      integration: serverlessLambdaIntegration,
+    });
+
+    // CfnOutput
+    new CfnOutput(this, 'api endpoint', {
+      value: serverlessLambdaApi.apiEndpoint,
+    });
   }
 }
