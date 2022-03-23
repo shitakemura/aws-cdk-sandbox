e612e4ea899de096ff4fff93fbac3d83d07c6fcb
shitakemura <shitakemura@gmail.com>
2022-03-23 17:20:37 +0900

    feat: create project

---
+++ aws-serverless-express/test/aws-serverless-express.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsServerlessExpress from '../lib/aws-serverless-express-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-serverless-express-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsServerlessExpress.AwsServerlessExpressStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
